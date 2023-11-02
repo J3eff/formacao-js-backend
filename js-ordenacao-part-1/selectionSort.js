aacf682afcd6773f3c9f3e9ff4fdc2912e8f735a
J3eff <jeffersonvideo125@gmail.com>
2023-11-02 16:17:08 -0300

    Programando o algoritmo

--- js-ordenacao-part-1/selectionSort.js
+++ js-ordenacao-part-1/selectionSort.js
@@ -5,10 +5,7 @@ for (let atual = 0; atual < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual);
 
     let livroAtual = livros[atual];
-    console.log('posição atual', atual)
-    console.log('livro atual', livroAtual)
     let livroMenorPreco = livros[menor];
-    console.log('livro menor preço', livroMenorPreco)
 
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
