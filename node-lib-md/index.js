3b834548d8fd17cabee29994804e6c6a29faa16c
J3eff <jeffersonvideo125@gmail.com>
2023-11-09 01:08:42 -0300

    'Promessas'

--- node-lib-md/index.js
+++ node-lib-md/index.js
@@ -2,18 +2,25 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 function trataErro(erro) {
-    console.log(erro)
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
 }
 
 function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro)
-            trataErro(erro);
-
-        console.log(chalk.green(texto))
-    })
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then(texto => console.log(chalk.green(texto)))
+        .catch(trataErro)
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro)
+//             trataErro(erro);
+
+//         console.log(chalk.green(texto))
+//     })
+// }
 
 pegaArquivo('./arquivos/texto.md')
