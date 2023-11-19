b7f39f6addebef375fd480fa8dc48e02719507f9
J3eff <jeffersonvideo125@gmail.com>
2023-11-19 10:34:47 -0300

    Classes e grupos

--- node-lib-md/index.js
+++ node-lib-md/index.js
@@ -16,17 +16,10 @@ async function pegaArquivo(caminhoDoArquivo) {
     }
 }
 
-
-//promises com then()
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then(texto => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
-
 pegaArquivo('./arquivos/texto.md')
 pegaArquivo('./arquivos/')
 
 //\[[^[\]]*?\]
+
+// \(https?:\/\/[^\s?#.].[^\s]*\)
+
