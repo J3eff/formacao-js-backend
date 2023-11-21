7d68ed2300380b90f3fa58b706ae07ef17f52bf9
J3eff <jeffersonvideo125@gmail.com>
2023-11-21 01:20:24 -0300

    Adicionando opções

--- node-lib-md/src/cli.js
+++ node-lib-md/src/cli.js
@@ -1,37 +1,46 @@
 import chalk from 'chalk';
 import pegaArquivo from './index.js';
 import fs from 'fs';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado, identificador = '') {
-    console.log(
-        chalk.yellow('Lista de links'), 
-        chalk.black.bgGreen(identificador),
-        resultado
-    );
+function imprimeLista(valida, resultado, identificador = '') {
+    if (valida) {
+        console.log(
+            chalk.yellow('Lista validada'),
+            chalk.black.bgGreen(identificador),
+            listaValidada(resultado));
+    } else {
+        console.log(
+            chalk.yellow('Lista de links'),
+            chalk.black.bgGreen(identificador),
+            resultado
+        );
+    }
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
 
     try {
         fs.lstatSync(caminho);
     } catch (erro) {
         if (erro.code === 'ENOENT') {
-            console.log('Arquivo ou diretório não existe'); 
-            return;           
+            console.log('Arquivo ou diretório não existe');
+            return;
         }
     }
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        imprimeLista(resultado);
+        imprimeLista(valida, resultado);
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async nomeDeArquivo => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            imprimeLista(lista, nomeDeArquivo);
+            imprimeLista(valida, lista, nomeDeArquivo);
         })
     }
 }
---
+++ node-lib-md/src/http-validacao.js
@@ -0,0 +1,4 @@
+export default function listaValidada(listaDeLinks) {
+    return 'Entrou na função';
+}
+
