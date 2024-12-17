33685ee3a05c0e3a5b887fa85111fd2eaede54e4
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-17 09:55:54 -0300

    retornanod as coisas

---
+++ js-ordenacao-part-2/aula3/quickSort.js
@@ -0,0 +1,42 @@
+const listaLivros = require('./array')
+const trocaLugar = require('./encontraMenores')
+
+function quickSort(array, esqueda, direita) {
+    if (array.length > 1) {
+        let indiceAtual = particiona(array, esqueda, direita);
+
+        if (esqueda < indiceAtual - 1)
+            quickSort(array, esqueda, indiceAtual - 1);
+
+        if (indiceAtual < direita)
+            quickSort(array, indiceAtual, direita)
+    }
+
+    return array;
+}
+
+function particiona(array, esqueda, direita) {
+    let pivo = array[Math.floor((esqueda + direita) / 2)]
+    let atualEsquerda = esqueda;
+    let atualDireita = direita;
+
+    while (atualEsquerda <= atualDireita) {
+        while (array[atualEsquerda].preco < pivo.preco) {
+            atualEsquerda++;
+        }
+
+        while (array[atualDireita].preco > pivo.preco) {
+            atualDireita--;
+        }
+
+        if (atualEsquerda <= atualDireita) {
+            trocaLugar(array, atualEsquerda, atualDireita);
+            atualEsquerda++;
+            atualDireita--;
+        }
+    }
+
+    return atualEsquerda;
+}
+
+console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
